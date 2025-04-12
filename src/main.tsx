611e2e65643427aa11603e91cc8ac4d61ec4f958
iduozee destiny <iduozeeosarugue027@gmail.com>
2025-04-12 05:34:11 -0400

    theme fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ThemeProvider } from './providers/ThemeProvider';
+import { Provider } from 'react-redux';
+import { store } from './store';
 import '@fontsource/inter/300.css';
 import '@fontsource/inter/400.css';
 import '@fontsource/inter/500.css';
@@ -9,6 +12,10 @@ import '@fontsource/inter/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
